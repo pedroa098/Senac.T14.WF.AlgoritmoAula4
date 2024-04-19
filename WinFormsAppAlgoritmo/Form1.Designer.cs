1a375345839fdfbad2abffe39a06d189898338ae
pedroa098 <pedro12345augusto098@gmail.com>
2024-04-18 21:57:15 -0300

    Criada tela de conta

--- WinFormsAppAlgoritmo/Form1.Designer.cs
+++ WinFormsAppAlgoritmo/Form1.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.Transparent;
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Segoe UI", 10F);
+            groupBox1.Location = new Point(30, 23);
+            groupBox1.Margin = new Padding(3, 2, 3, 2);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(3, 2, 3, 2);
+            groupBox1.Size = new Size(952, 295);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Cadastro de Conta";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(34, 214);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(405, 34);
+            textBox3.TabIndex = 8;
+            textBox3.Text = "1000";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(466, 214);
+            textBox4.Margin = new Padding(3, 2, 3, 2);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(405, 34);
+            textBox4.TabIndex = 7;
+            textBox4.Text = "Corrente";
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(466, 167);
+            label4.Name = "label4";
+            label4.Size = new Size(135, 28);
+            label4.TabIndex = 6;
+            label4.Text = "Tipo da Conta";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(34, 167);
+            label3.Name = "label3";
+            label3.Size = new Size(146, 28);
+            label3.TabIndex = 4;
+            label3.Text = "Saldo da Conta";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(466, 93);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(405, 34);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "Pedro Augusto Hochscheidt";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(466, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(151, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Titular da Conta";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.White;
+            textBox1.Location = new Point(34, 93);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(405, 34);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "123123";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Location = new Point(34, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(168, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Numero da Conta";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gainsboro;
+            ClientSize = new Size(1030, 352);
+            Controls.Add(groupBox1);
+            Font = new Font("Bell MT", 11F);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox4;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox3;
     }
 }
--- WinFormsAppAlgoritmo/Form1.cs
+++ WinFormsAppAlgoritmo/Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormsAppAlgoritmo
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
